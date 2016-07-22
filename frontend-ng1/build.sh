c04a35c55e268b30ee25f27569d4eaf59a63b5b5
eri <alex-eri@ya.ru>
2016-07-22 11:29:46 +0300

    builder

---
+++ frontend-ng1/build.sh
@@ -0,0 +1,21 @@
+TARGET=`pwd`/build/mikrotik
+mkdir -p ${TARGET}
+install -d ${TARGET}/js
+install -d ${TARGET}/style
+install -d ${TARGET}/style/css
+install -d ${TARGET}/style/fonts
+install -d ${TARGET}/partials
+install -d ${TARGET}/templates
+pushd mikrotik/
+install *.html ${TARGET}
+install config.json ${TARGET}
+install js/app.js ${TARGET}/js/
+install js/jquery/dist/jquery.min.js ${TARGET}/js/
+install js/angular/angular.min.js ${TARGET}/js/
+install js/angular-resource/angular-resource.min.js ${TARGET}/js/
+install js/angular-animate/angular-animate.min.js ${TARGET}/js/
+install style/fonts/* ${TARGET}/style/fonts/
+install style/css/bootstrap.min.css ${TARGET}/style/css/
+install templates/*.html ${TARGET}/templates/
+install partials/*.html ${TARGET}/partials/
+popd
