cb27049161c9c98662a86cfdfa127add39afbdb8
eri <alex-eri@ya.ru>
2017-03-12 17:47:04 +0300

    flows

--- server/reindex.py
+++ server/reindex.py
@@ -22,6 +22,7 @@ def index(config):
         db.accounting.ensure_index([ ("callee",1) ], unique=False),
         db.voucher.ensure_index( [ ("voucher",1), ("callee",1),("closed",1) ], unique=True, sparse=True ),
         db.voucher.ensure_index( [ ("series",1)], unique=False),
+        db.collector.ensure_index( [ ("first",1)], unique=False),
     ]
     tasks = [ asyncio.ensure_future(t) for t in tasks ]
     storage.logger.info('reindexing')
