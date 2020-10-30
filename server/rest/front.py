a8ece05198d7e71692a8b88c515e8356559f2fc2
Aleksandr Stepanov <sa@eri.su>
2020-10-30 15:50:45 +0300

    fix profiles

--- server/rest/front.py
+++ server/rest/front.py
@@ -1,14 +1,15 @@
 from .decorators import json
 import os
 from aiohttp import web
+import string
 
 async def get_uam_config(db,profile):
     profile = str(profile)
-    for c in '-:_':
+    for c in string.punctuation:
         profile = profile.replace(c,'')
 
     default = await db.uamconfig.find_one({'_id':'default'})
-    if default and profile.isidentifier():
+    if default:
         conf = await db.uamconfig.find_and_modify({'_id': profile}, {'$setOnInsert': { 'auto': True }}, upsert=True, new=False)
         if conf:
             conf = {k: v for k, v in conf.items() if v or v is False or v == 0 }
