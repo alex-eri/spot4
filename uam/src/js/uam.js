195da15341303cc54e607f927b9a3984f79254c4
Aleksandr Stepanov <sa@eri.su>
2021-10-20 03:42:28 +0300

    location

--- uam/src/js/uam.js
+++ uam/src/js/uam.js
@@ -712,8 +712,8 @@ app.controller('Check',  ['$rootScope','$resource','$cookies','$location', '$win
 
     }]);
 
-app.controller('Status',  ['$rootScope','$resource','$cookies', '$location',
-    function ( $scope, $resource, $cookies, $location ){
+app.controller('Status',  ['$rootScope','$resource','$cookies', '$location', '$window', 
+    function ( $scope, $resource, $cookies, $location, $window ){
 
     var ischilli = $cookies.get('uamip');
 
